2f0113c9c5803d029e279a7ca5ea330654a76b0d
pietrotota <pietro.tota@gmail.com>
2016-11-07 20:11:10 +0000

    Initial commit

---
+++ app/src/main/java/tota_tagliente_riccardi/it/smartagricolture/MainActivity.java
@@ -0,0 +1,13 @@
+package tota_tagliente_riccardi.it.smartagricolture;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
