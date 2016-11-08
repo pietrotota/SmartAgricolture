31d34f1caaac967c842653ff28f3df886d811131
Luke <pippoluca90@hotmail.it>
2016-11-08 18:50:32 +0100

    prova

--- sensorlib/src/main/java/com/ttr/sensorlib/SensorActivity.java
+++ sensorlib/src/main/java/com/ttr/sensorlib/SensorActivity.java
@@ -1,8 +1,13 @@
 package com.ttr.sensorlib;
 
+import android.hardware.SensorManager;
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 /**
  * Created by Luke on 08/11/2016.
  */
 
 public class SensorActivity extends Activity implements SensorEventListener {
+
 }
