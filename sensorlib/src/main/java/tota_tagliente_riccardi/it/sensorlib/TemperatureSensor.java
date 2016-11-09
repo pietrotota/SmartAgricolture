c327e12258cc81921bdab8c957316f1ef4b923bb
Luke <pippoluca90@hotmail.it>
2016-11-09 19:30:31 +0100

    upload classi dei vari sensori con l'handler che cattura il dato quando cambia.

--- sensorlib/src/main/java/tota_tagliente_riccardi/it/sensorlib/TemperatureSensor.java
+++ sensorlib/src/main/java/tota_tagliente_riccardi/it/sensorlib/TemperatureSensor.java
@@ -1,9 +1,36 @@
 package tota_tagliente_riccardi.it.sensorlib;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+
 public class TemperatureSensor extends SensorHandler<Float> {
+
+    float cTemp=0;
+ 
+    Sensor mTemp= mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);;
+
+    // In questo modo recupero il nome di default del sensore
+    String sensorName= mTemp.getName();
+
 
     @Override
     public Float getData() {
-        return 0f;
+        return cTemp;
     }
+
+    @Override
+    //aggiorna il dato col valore più corrente quando il dato è disponibile sul sensore, ovvero quando cambia la luminosità ad esempio
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        cTemp=sensorEvent.values[0];
+    }
+
+    @Override
+
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+
+
 }
